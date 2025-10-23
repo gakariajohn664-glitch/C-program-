239562bde9e1be55807c32f1283c3bd9919c0b07
Boro John <gakariajohn664@gmail.com>
2025-10-23 23:47:19 +0300

    Add files via upload

---
+++ function of bill.c
@@ -0,0 +1,28 @@
+/*
+Name: Boro John Gakaria
+Reg:CT100/G/26190/25
+Description:function to calculate bill
+*/
+#include<stdio.h>
+//  function prototype
+int bill(int unit,int amount);
+
+void main()
+{
+	int bill_1,bill_2,bill_3;
+	bill_1=bill(100,10);
+	bill_2=bill(100,15);
+	bill_3=bill(200,20);
+	printf("The bill 1is= ksh:%d\n",bill);
+	printf("The bill 2 is= ksh:%d\n",bill);
+	printf("The bill3 is=ksh:%d\n",bill);
+}
+
+	//function declaration
+	int bill(int unit,int amount)
+	{
+int bill(int unit,int amount);
+int total_bill;
+total_bill= unit* amount;
+	}
+	
